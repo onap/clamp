12d94fcae13c5adc9d26a955b3dc8b38f8100e2c
Anaël Closson <ac2550@intl.att.com>
2017-09-06 14:13:11 +0200

    Missing copyright modification statement
    Change-Id: Id186bb9a62e43d81ad53a4e4d091e1d18e3015a1
    Issue-Id: CLAMP-43
    Signed-off-by: Anaël Closson <ac2550@intl.att.com>

--- src/main/resources/META-INF/resources/designer/lib/multiselect.js
+++ src/main/resources/META-INF/resources/designer/lib/multiselect.js
@@ -3,6 +3,7 @@
  *
  * Apache License, Version 2.0:
  * Copyright (c) 2012 - 2015 David Stutz
+ * Modifications copyright (C) 2017 AT&T Intellectual Property
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a
