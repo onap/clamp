70b3aec5adcdec71b18bb01368dba4581f10c777
xg353y <xg353y@intl.att.com>
2018-05-18 17:59:06 +0200

    Generate API documents
    Adding maven plugins to scan Clamp code automatically and generate
    swagger.json, html and pdf documents with the exposed Clamp API
    information.
    
    Issue-ID: CLAMP-169
    Change-Id: Ia46cd2aa7000251b30c9eaaa040f1f8d9246af45
    Signed-off-by: xg353y <xg353y@intl.att.com>

--- src/main/java/org/onap/clamp/clds/service/JaxrsApplication.java
+++ src/main/java/org/onap/clamp/clds/service/JaxrsApplication.java
@@ -26,21 +26,14 @@ package org.onap.clamp.clds.service;
 import com.att.eelf.configuration.EELFLogger;
 import com.att.eelf.configuration.EELFManager;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.stereotype.Component;
-
 @Component
 @ApplicationPath("/restservices/clds/v1")
 public class JaxrsApplication extends Application {
@@ -54,20 +47,4 @@ public class JaxrsApplication extends Application {
             return Optional.empty();
         }
     };
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new HashSet<>();
-        resources.add(JacksonObjectMapperProvider.class);
-        resources.add(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
-        resources.addAll(scan());
-        return resources;
-    }
-
-    private List<Class<?>> scan() {
-        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
-        scanner.addIncludeFilter(new AnnotationTypeFilter(javax.ws.rs.Path.class));
-        return scanner.findCandidateComponents("org.onap.clamp.clds").stream().map(beanDefinitionToClass)
-                .filter(Optional::isPresent).map(Optional::get).collect(Collectors.toList());
-    }
 }
