b2aa809cc409c707938ede8e9714f8d681e07272
sebdet <sebastien.determe@intl.att.com>
2019-03-21 13:56:08 +0100

    Move Javascript code
    Move js code outstide of the html page so that we can debug  it
    
    Issue-ID: CLAMP-330
    Change-Id: Iba5a61f7a001f0bd73f99b340380b5d6a1383831
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- src/main/resources/META-INF/resources/designer/scripts/app.js
+++ src/main/resources/META-INF/resources/designer/scripts/app.js
@@ -677,7 +677,7 @@ function($scope, $rootScope, $timeout, dialogs) {
 	    $scope.PolicyWindow = function(policy) {
 
 		    var dlg = dialogs.create(
-		    'partials/portfolios/PolicyWindow_properties.html',
+		    'partials/portfolios/operational_policy_window.html',
 		    'operationalPolicyCtrl', {
 		        closable : true,
 		        draggable : true
