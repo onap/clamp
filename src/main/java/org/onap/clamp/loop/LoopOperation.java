f00087f5118c1525c5538fd555f5469f2007422d
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-04-29 12:33:40 +0200

    Fixed Checkstyle issues
    Fixed most audit checkstyle issues
    
    Change-Id: I5b6de1ac602e9a6aeec59e12eb97ce716cfe55b4
    Issue-ID: CLAMP-328
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>

--- src/main/java/org/onap/clamp/loop/LoopOperation.java
+++ src/main/java/org/onap/clamp/loop/LoopOperation.java
@@ -97,6 +97,7 @@ public class LoopOperation {
         rootObject.addProperty(DCAE_SERVICETYPE_ID, serviceTypeId);
         if (bluePrint != null) {
             rootObject.add(DCAE_INPUTS, bluePrint);
+
         }
         String apiBodyString = rootObject.toString();
         logger.info("Dcae api Body String - " + apiBodyString);
@@ -144,7 +145,6 @@ public class LoopOperation {
 
         // use http4 instead of http, because camel http4 component is used to do the http call
         statusUrl.replace("http", "http4");
-
         loop.setDcaeDeploymentId(deploymentId);
         loop.setDcaeDeploymentStatusUrl(statusUrl);
         loopService.saveOrUpdateLoop(loop);
