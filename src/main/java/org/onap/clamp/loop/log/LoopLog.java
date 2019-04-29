a194daf7d6e7caf13428e1b589278711632eec46
m.kowalski3 <m.kowalski3@partner.samsung.com>
2019-04-29 12:33:40 +0200

    Fixed Checkstyle issues
    Fixed most audit checkstyle issues
    
    Change-Id: I5b6de1ac602e9a6aeec59e12eb97ce716cfe55b4
    Issue-ID: CLAMP-328
    Signed-off-by: Marcin Kowalski <m.kowalski3@partner.samsung.com>

--- src/main/java/org/onap/clamp/loop/log/LoopLog.java
+++ src/main/java/org/onap/clamp/loop/log/LoopLog.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2019 AT&T Intellectual Property. All rights
  *                             reserved.
  * ================================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -83,6 +85,12 @@ public class LoopLog implements Serializable {
     public LoopLog() {
     }
 
+    /**
+     * Constructor.
+     * @param message message
+     * @param logType logType
+     * @param loop loop
+     */
     public LoopLog(String message, LogType logType, Loop loop) {
         this.message = message;
         this.logType = logType;
