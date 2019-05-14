3ecf12c7359849cdbd33d7008067aed610b2a119
k.kedron <k.kedron@partner.samsung.com>
2019-05-14 14:38:04 +0200

    Improve test for DictionaryService.
    Add new test for CldsDictionaryService.
    Refactor createOrUpdateDictionary method.
    Remove useless final from private method.
    Checkstyle issue.
    
    Change-Id: I26e782aeb4a7429d342f90d4231dd24cca2e152f
    Issue-ID: CLAMP-355
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>

--- src/test/java/org/onap/clamp/clds/it/CldsDictionaryServiceItCase.java
+++ src/test/java/org/onap/clamp/clds/it/CldsDictionaryServiceItCase.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2018 AT&T Intellectual Property. All rights
  *                             reserved.
  * ================================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -29,7 +31,6 @@ import static org.junit.Assert.assertNotNull;
 import com.att.eelf.configuration.EELFLogger;
 import com.att.eelf.configuration.EELFManager;
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -69,17 +70,15 @@ public class CldsDictionaryServiceItCase {
     private Authentication authentication;
     private CldsDictionary cldsDictionary;
     private CldsDictionaryItem cldsDictionaryItem;
-    private List<GrantedAuthority> authList = new LinkedList<GrantedAuthority>();
+    private List<GrantedAuthority> authList = new LinkedList<>();
     private LoggingUtils util;
 
     /**
      * Setup the variable before the tests execution.
      *
-     * @throws IOException
-     *         In case of issues when opening the files
      */
     @Before
-    public void setupBefore() throws IOException {
+    public void setupBefore() {
         authList.add(new SimpleGrantedAuthority("permission-type-cl|dev|read"));
         authList.add(new SimpleGrantedAuthority("permission-type-cl|dev|update"));
         authList.add(new SimpleGrantedAuthority("permission-type-template|dev|read"));
@@ -101,8 +100,8 @@ public class CldsDictionaryServiceItCase {
         cldsDictionary = new CldsDictionary();
 
         cldsDictionary.setDictionaryName("TestDictionary");
-        ResponseEntity entity = cldsDictionaryService.createOrUpdateDictionary("TestDictionary", cldsDictionary);
-        cldsDictionary = (CldsDictionary) entity.getBody();
+        ResponseEntity<CldsDictionary> entity = cldsDictionaryService.createOrUpdateDictionary(cldsDictionary);
+        cldsDictionary = entity.getBody();
 
         cldsDictionaryItem = new CldsDictionaryItem();
         cldsDictionaryItem.setDictElementShortName("TestDictionaryItemShortName");
@@ -116,9 +115,18 @@ public class CldsDictionaryServiceItCase {
     }
 
     @Test
-    public void testCreateOrUpdateDictionary() throws Exception {
-        ResponseEntity<CldsDictionary> responseEntity = cldsDictionaryService.createOrUpdateDictionary("TestDictionary",
-            cldsDictionary);
+    public void testCreateDefaultDictionary(){
+        String dictionaryName = "TestDefaultDictionary";
+        ResponseEntity<CldsDictionary> entity = cldsDictionaryService.createOrUpdateDictionary(dictionaryName);
+        CldsDictionary dictionary = entity.getBody();
+        assertNotNull(dictionary);
+        logger.info("CLDS Default Dictionary is:" + dictionary);
+        assertEquals(dictionaryName, dictionary.getDictionaryName());
+    }
+
+    @Test
+    public void testCreateOrUpdateDictionary() {
+        ResponseEntity<CldsDictionary> responseEntity = cldsDictionaryService.createOrUpdateDictionary(cldsDictionary);
         CldsDictionary dictionary = responseEntity.getBody();
         assertNotNull(dictionary);
         logger.info("CLDS Dictionary is:" + dictionary);
@@ -126,7 +134,7 @@ public class CldsDictionaryServiceItCase {
     }
 
     @Test
-    public void testCreateOrUpdateDictionaryElements() throws Exception {
+    public void testCreateOrUpdateDictionaryElements() {
         cldsDictionaryItem = new CldsDictionaryItem();
         cldsDictionaryItem.setDictElementShortName("TestDictionaryItemShortName1");
         cldsDictionaryItem.setDictElementName("TestDictionaryItemName1");
@@ -135,7 +143,7 @@ public class CldsDictionaryServiceItCase {
         cldsDictionaryItem.setDictElementDesc("TestDictionaryItemDesc1");
 
         ResponseEntity<CldsDictionaryItem> responseEntity = cldsDictionaryService
-            .createOrUpdateDictionaryElements("TestDictionary", cldsDictionaryItem);
+                .createOrUpdateDictionaryElements("TestDictionary", cldsDictionaryItem);
         CldsDictionaryItem dictionaryItem = responseEntity.getBody();
         assertNotNull(dictionaryItem);
         logger.info("CLDS Dictionary Item is:" + dictionaryItem);
@@ -143,7 +151,7 @@ public class CldsDictionaryServiceItCase {
     }
 
     @Test
-    public void testGetAllDictionaryNames() throws Exception {
+    public void testGetAllDictionaryNames() {
         ResponseEntity<List<CldsDictionary>> responseEntity = cldsDictionaryService.getAllDictionaryNames();
         List<CldsDictionary> dictionaries = responseEntity.getBody();
         assertNotNull(dictionaries);
@@ -151,9 +159,9 @@ public class CldsDictionaryServiceItCase {
     }
 
     @Test
-    public void testGetDictionaryElementsByName() throws Exception {
+    public void testGetDictionaryElementsByName() {
         ResponseEntity<List<CldsDictionaryItem>> responseEntity = cldsDictionaryService
-            .getDictionaryElementsByName("TestDictionary");
+                .getDictionaryElementsByName("TestDictionary");
         List<CldsDictionaryItem> dictionaryItems = responseEntity.getBody();
         assertNotNull(dictionaryItems);
         logger.info("CLDS Dictionary Item LIst is:" + dictionaryItems);
