2249cf39e5ea07969a35754fc72db40d6de90904
sebdet <sebastien.determe@intl.att.com>
2019-06-14 11:15:09 +0200

    change required for new release
    Change required for new release management
    
    Issue-ID: CLAMP-409
    Change-Id: I36b4dc4b8c653cdd5a5a59941b3a4659774cec00
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>

--- src/test/java/org/onap/clamp/flow/FlowLogOperationTestItCase.java
+++ src/test/java/org/onap/clamp/flow/FlowLogOperationTestItCase.java
@@ -1,4 +1,29 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
 package org.onap.clamp.flow;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
@@ -10,9 +35,6 @@ import org.onap.clamp.clds.util.ONAPLogConstants;
 import org.onap.clamp.flow.log.FlowLogOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-
 
 public class FlowLogOperationTestItCase {
 
@@ -21,19 +43,19 @@ public class FlowLogOperationTestItCase {
 
     @Test
     public void testStratLog() {
-        //given
+        // given
         FlowLogOperation flowLogOperation = new FlowLogOperation();
         Exchange exchange = new DefaultExchange(camelContext);
         LoggingUtils loggingUtils = mock(LoggingUtils.class);
         ReflectionTestUtils.setField(flowLogOperation, "util", loggingUtils);
 
-        //when
+        // when
         Mockito.when(loggingUtils.getProperties(ONAPLogConstants.MDCs.REQUEST_ID)).thenReturn("MockRequestId");
         Mockito.when(loggingUtils.getProperties(ONAPLogConstants.MDCs.INVOCATION_ID)).thenReturn("MockInvocationId");
         Mockito.when(loggingUtils.getProperties(ONAPLogConstants.MDCs.PARTNER_NAME)).thenReturn("MockPartnerName");
         flowLogOperation.startLog(exchange, "serviceName");
 
-        //then
+        // then
         assertThat(exchange.getProperty(ONAPLogConstants.Headers.REQUEST_ID)).isEqualTo("MockRequestId");
         assertThat(exchange.getProperty(ONAPLogConstants.Headers.INVOCATION_ID)).isEqualTo("MockInvocationId");
         assertThat(exchange.getProperty(ONAPLogConstants.Headers.PARTNER_NAME)).isEqualTo("MockPartnerName");
