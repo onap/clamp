e5f7ef5d2c6043dc002a622611ec1a372ea6cf46
drveerendra <vrajasekharaiah@att.com>
2019-11-11 19:37:39 -0500

    Adding View BP template Menu
    Adding View Blue Print templates option under Template menu. Adding few
    more test cases for ViewTosca Modals to cover the api calls.
    
    Issue-ID: CLAMP-544
    Change-Id: Ibf881f46fd03e9149a88ccc211a996d99fd74748
    Signed-off-by: drveerendra <vrajasekharaiah@att.com>

--- ui-react/src/api/TemplateMenuService.js
+++ ui-react/src/api/TemplateMenuService.js
@@ -21,20 +21,154 @@
  */
 
 export default class TemplateMenuService {
-  static getToscaModels() {
+  static getToscaPolicyModels() {
     return fetch('restservices/clds/v2/loop/tosca/models', { method: 'GET', credentials: 'same-origin' })
       .then(function (response) {
-        console.debug("getToscaModels response received: ", response.status);
+        console.debug("getToscaPolicyModels response received: ", response.status);
         if (response.ok) {
           return response.json();
         } else {
-          console.error("getToscaModels query failed");
+          console.error("getToscaPolicyModels query failed");
           return {};
         }
       })
       .catch(function (error) {
-        console.error("getToscaModels error received", error);
+        console.error("getToscaPolicyModels error received", error);
         return {};
+      });
+  }
+
+  static getBlueprintMicroServiceTemplates() {
+    return fetch('restservices/clds/v2/loop/templates', { method: 'GET', credentials: 'same-origin', })
+      .then(function (response) {
+        console.debug("getBlueprintMicroServiceTemplates response received: ", response.status);
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.error("getBlueprintMicroServiceTemplates query failed");
+          return {};
+        }
+      })
+      .catch(function (error) {
+        console.error("getBlueprintMicroServiceTemplates error received", error);
+        return {};
+      });
+  }
+
+  static getDictionary() {
+    return fetch('restservices/clds/v2/loop/dictionary/', { method: 'GET', credentials: 'same-origin', })
+      .then(function (response) {
+        console.debug("getDictionary response received: ", response.status);
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.error("getDictionary query failed");
+          return {};
+        }
+      })
+      .catch(function (error) {
+        console.error("getDictionary error received", error);
+        return {};
+      });
+  }
+
+  static getDictionaryItems(dictionaryName) {
+    return fetch('restservices/clds/v2/loop/dictionary/' + dictionaryName + '/items', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: 'same-origin',
+    })
+      .then(function (response) {
+        console.debug("getDictionaryItems response received: ", response.status);
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.error("getDictionaryItems query failed");
+          return {};
+        }
+      })
+      .catch(function (error) {
+        console.error("getDictionaryItems error received", error);
+        return {};
+      });
+  }
+
+  static uploadToscaModal(toscaModelName, jsonData) {
+    var svcRequest = {toscaModelName: toscaModelName, toscaModelRevisions: [{toscaModelYaml: jsonData}]};
+    return fetch('/restservices/clds/v2/loop/tosca/models/' + toscaModelName, {
+      method: 'PUT',
+      credentials: 'same-origin',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(svcRequest)
+    })
+      .then(function (response) {
+        console.debug("uploadToscaModal response received: ", response.status);
+        if (response.ok) {
+          var message = {status: response.status, message: 'Tosca Model successfully uploaded'};
+          return message;
+        } else {
+        	console.error("uploadToscaModel failed");
+			    return response.text();
+        }
+      })
+      .catch(function (error) {
+        console.error("uploadToscaModal error received", error);
+        return "";
+      });
+  }
+
+  static uploadBPTempl(templateName, jsonData) {
+    var svcRequest = {templateName: templateName, templateYaml: jsonData};
+    return fetch('/restservices/clds/v2/loop/createLoopTemplate/' + templateName, {
+      method: 'PUT',
+      credentials: 'same-origin',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(svcRequest)
+    })
+      .then(function (response) {
+        console.debug("uploadBPTempl response received: ", response.status);
+        if (response.ok) {
+          var message = {status: response.status, message: 'BP template successfully uploaded'};
+          return message;
+        } else {
+          console.error("uploadBPTempl query failed");
+          return response.text();
+        }
+      })
+      .catch(function (error) {
+        console.error("uploadBPTempl error received", error);
+        return "";
+      });
+  }
+
+  static insDictionary(jsonData) {
+    return fetch('/restservices/clds/v2/loop/dictionary', {
+      method: 'PUT',
+      credentials: 'same-origin',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData)
+    })
+      .then(function (response) {
+        console.debug("insDictionary response received: ", response.status);
+        if (response.ok) {
+          return response.status;
+        } else {
+          var errorMessage = response.status;
+          console.error("insDictionary query failed", response.status);
+          return errorMessage;
+        }
+      })
+      .catch(function (error) {
+        console.error("insDictionary error received", error);
+        return "";
       });
   }
 }
