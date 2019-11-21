f5ccdcfbbcc3b08e20f8e68a3b62efe2d99390ed
nrpandya <neenendra.pandya@att.com>
2019-11-21 11:51:18 -0600

    Add template and tosca model entities and repositories
    Add Control loop template, tosca model and model policy properties
    hibernate entity classes and crud repositories
    
    Issue-ID: CLAMP-555
    Change-Id: Ib7f07aca5ad2ddf5caff7c98ea9341bdc147e817
    Signed-off-by: nrpandya <neenendra.pandya@att.com>

---
+++ src/main/java/org/onap/clamp/loop/entity/LoopTemplate.java
@@ -0,0 +1,168 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.loop.entity;
+
+
+import com.google.gson.annotations.Expose;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.onap.clamp.dao.model.jsontype.StringJsonUserType;
+
+@Entity
+@Table(name = "loop_template")
+@TypeDefs({ @TypeDef(name = "json", typeClass = StringJsonUserType.class) })
+public class LoopTemplate implements Serializable {
+	    
+	/**
+     * The serial version id.
+     */
+    private static final long serialVersionUID = -286522707701388642L;
+
+	
+    @Expose
+    @Column(nullable = false, name = "template_name", unique = true)
+    private String templateName;
+
+    @Id
+	@Expose
+    @Column(nullable = false, name = "template_id", unique = true)
+	@GeneratedValue(generator="system-uuid")	
+	@GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String templateId;
+
+    @Expose
+    @Column(columnDefinition = "MEDIUMTEXT", name = "template_yaml")
+    private String templateYaml;
+
+    @Expose
+    @Column(columnDefinition = "MEDIUMTEXT", name = "svg_image")
+    private String templateImage;
+    
+    @Expose
+    @Column(name = "updated_by")
+    private String updatedBy;
+   
+    @Expose
+    @UpdateTimestamp
+    @Column(name = "timestamp")
+    private  Timestamp timestamp;
+    
+    
+    @Expose
+    @OneToMany(mappedBy="loopTemplate", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+     private Set<TemplatePolicy> templatePolicy = new HashSet<>();
+
+	public Set<TemplatePolicy> getTemplatePolicy() {
+		return templatePolicy;
+	}
+
+	public void setTemplatePolicy(Set<TemplatePolicy> templatePolicy) {
+		this.templatePolicy = templatePolicy;
+	}
+	
+    public String getTemplateName() {
+		return templateName;
+	}
+
+	public void setTemplateName(String templateName) {
+		this.templateName = templateName;
+	}
+
+	public String getTemplateId() {
+		return templateId;
+	}
+
+	public void setTemplateId(String templateId) {
+		this.templateId = templateId;
+	}
+
+	public String getTemplateYaml() {
+		return templateYaml;
+	}
+
+	public void setTemplateYaml(String templateYaml) {
+		this.templateYaml = templateYaml;
+	}
+
+	public String getTemplateImage() {
+		return templateImage;
+	}
+
+	public void setTemplateImage(String templateImage) {
+		this.templateImage = templateImage;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+    public LoopTemplate() {
+		
+	}
+	
+	public LoopTemplate(String templateName, String templateId, String templateYaml, String templateImage,
+			String updatedBy, Timestamp timestamp) {
+		super();
+		this.templateName = templateName;
+		this.templateId = templateId;
+		this.templateYaml = templateYaml;
+		this.templateImage = templateImage;
+		this.updatedBy = updatedBy;
+		this.timestamp = timestamp;
+	}
+
+}
---
+++ src/main/java/org/onap/clamp/loop/entity/TemplatePolicy.java
@@ -0,0 +1,98 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.loop.entity;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import com.google.gson.annotations.Expose;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.onap.clamp.dao.model.jsontype.StringJsonUserType;
+
+
+@Entity
+@Table(name = "loop_template_policy")
+@TypeDefs({ @TypeDef(name = "json", typeClass = StringJsonUserType.class) })
+public class TemplatePolicy implements Serializable {
+    /**
+     * The serial version ID.
+     */
+    private static final long serialVersionUID = 6117076450841538255L;
+
+    @Transient
+    private static final EELFLogger logger = EELFManager.getInstance().getLogger(TemplatePolicy.class);
+ 
+    @Id
+    @Expose
+    @Column(nullable = false, name = "template_policy_id", unique = true)
+    private String templatePolicyId;
+    
+    //@Id	
+	@Expose
+    @Column(name = "policy_model_id")
+    private String policyModelId;
+	
+	@ManyToOne
+    @JoinColumn(name = "template_id")
+    private LoopTemplate loopTemplate;
+	
+    
+    public LoopTemplate getLoopTemplate() {
+		return loopTemplate;
+	}
+
+	public void setLoopTemplate(LoopTemplate loopTemplate) {
+		this.loopTemplate = loopTemplate;
+	}
+
+	public String getPolicyModelId() {
+		return policyModelId;
+	}
+
+	public void setPolicyModelId(String policyModelId) {
+		this.policyModelId = policyModelId;
+	}
+
+	public TemplatePolicy() {
+        // Serialization
+    }
+    
+    public TemplatePolicy(LoopTemplate loopTemplate, String policyModelId, String templatePolicyId) {
+        this.loopTemplate = loopTemplate;
+        this.policyModelId = policyModelId;
+        this.templatePolicyId = templatePolicyId;
+    }
+
+   
+}
