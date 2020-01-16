1192540a1de20e33a237605d3689dd94603d5ac9
pk3428 <praveen.kumbham@att.com>
2020-01-16 23:03:29 +0000

    Backend changes to support Template APIs used by Frontend.
    Changes to Camel route endpoints and supporting service and repository
    layers needed by the APIs.
    
    Issue-ID: CLAMP-587
    Change-Id: I038b88a5eeb49963403baedd5c5fbb5ce2195b02
    Signed-off-by: pk3428 <praveen.kumbham@att.com>

---
+++ src/main/java/org/onap/clamp/loop/service/LoopTemplateService.java
@@ -0,0 +1,74 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.loop.service;
+
+import java.util.List;
+
+import org.onap.clamp.loop.template.LoopTemplate;
+import org.onap.clamp.clds.service.SecureServiceBase;
+import org.onap.clamp.loop.template.LoopTemplatesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service
+public class LoopTemplateService extends SecureServiceBase {
+	
+	@Value("${clamp.config.security.permission.type.tosca:permission-type-tosca}")
+    private String                  cldsPermissionTypeTosca;
+    @Value("${clamp.config.security.permission.instance:dev}")
+    private String                  cldsPermissionInstance;   
+
+	private final LoopTemplatesRepository loopTemplatesRepository;
+	   
+	/**
+     * Constructor.
+     */
+	@Autowired
+	public LoopTemplateService(LoopTemplatesRepository loopTemplatesRepository) {
+        this.loopTemplatesRepository = loopTemplatesRepository;
+    }
+
+    LoopTemplate saveOrUpdateLoop(LoopTemplate loopTemplate) {
+        return loopTemplatesRepository.save(loopTemplate);
+    }
+
+    public List<String> getLoopTemplateNames() {
+        return loopTemplatesRepository.getAllLoopTemplateNames();
+    }
+    
+    public List<LoopTemplate> getAllLoopTemplates() {    
+        return (List<LoopTemplate>)loopTemplatesRepository.findAll();
+    }
+
+    public LoopTemplate getLoopTemplate(String loopTemplateName) {
+       return loopTemplatesRepository.findById(loopTemplateName).orElse(null);
+    }
+
+    public void deleteLoopTemplate(String loopTemplateName) {
+    	loopTemplatesRepository.deleteById(loopTemplateName);
+    }
+}
