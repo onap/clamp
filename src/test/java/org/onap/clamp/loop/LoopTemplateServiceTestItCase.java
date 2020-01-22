7c23fdc6a44d66934d6cfc5ad9b2ae17a9bc779c
pk3428 <praveen.kumbham@att.com>
2020-01-22 06:51:48 +0000

    Backend changes to support Template APIs used by Frontend.
    Fixed build failure issue
    
    Issue-ID: CLAMP-587
    Change-Id: I0fddda361b124e1de4f1da70269438b8800f97fa
    Signed-off-by: pk3428 <praveen.kumbham@att.com>

---
+++ src/test/java/org/onap/clamp/loop/LoopTemplateServiceTestItCase.java
@@ -0,0 +1,82 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 Nokia Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.clamp.loop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onap.clamp.clds.Application;
+import org.onap.clamp.loop.log.LoopLogService;
+import org.onap.clamp.loop.service.LoopTemplateService;
+import org.onap.clamp.loop.template.LoopTemplate;
+import org.onap.clamp.loop.template.LoopTemplatesRepository;
+import org.onap.clamp.policy.microservice.MicroServicePolicyService;
+import org.onap.clamp.policy.operational.OperationalPolicyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class LoopTemplateServiceTestItCase {
+
+    @Autowired
+    LoopTemplateService loopTemplateService;
+
+    @Autowired
+    LoopTemplatesRepository loopTemplatesRepository;
+
+    @Autowired
+    MicroServicePolicyService microServicePolicyService;
+
+    @Autowired
+    OperationalPolicyService operationalPolicyService;
+
+    @Autowired
+    LoopLogService loopLogService;
+
+    @Test
+    public void testGetLoopTemplateNames() {
+              
+        List<String> tnames = loopTemplateService.getLoopTemplateNames();      
+        // then
+        assertThat(tnames).isNotNull();
+        assertThat(tnames).size().isGreaterThanOrEqualTo(0);
+        
+    }
+    
+    @Test
+    
+    public void testGetAllLoopTemplates() {
+        List<LoopTemplate> loopTemplates = loopTemplateService.getAllLoopTemplates();      
+        // then
+        assertThat(loopTemplates).isNotNull();
+        assertThat(loopTemplates).size().isGreaterThanOrEqualTo(0);
+    }
+    
+   
+}
