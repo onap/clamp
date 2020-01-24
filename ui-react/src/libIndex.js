221dc0c9a6ebcc9b4580f04f9f492601e7a2ebf2
TedHumphrey <Thomas.Humphrey@att.com>
2020-01-24 01:27:43 -0500

    Changes for populating ReactJS component library
    Modification of Maven pom.xml to rollup the source files and
    publish the library
    Introduction of OnapConstants.js to provide single focus for defining
    constants shared across multiple components; resolves currrent circular
    dependency between LoopUI.js and MenuBar.js
    Corrected LoopSvg.js to import withRoute from react-router-dom instead
    of react-router
    
    Issue-ID: CLAMP-586
    Change-Id: If657c645ac90f1d6305464851d2943b666f0aa9e
    Signed-off-by: TedHumphrey <Thomas.Humphrey@att.com>

---
+++ ui-react/src/libIndex.js
@@ -0,0 +1,46 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+export { default as LoopSvg } from './components/loop_viewer/svg/LoopSvg';
+
+export { default as ConfigurationPolicyModal } from './components/dialogs/ConfigurationPolicy/ConfigurationPolicyModal';
+export { default as DeployLoopModal } from './components/dialogs/Loop/DeployLoopModal';
+export { default as LoopActionService } from './api/LoopActionService';
+export { default as LoopCache }  from './api/LoopCache';
+export { default as LoopComponentConverter } from './components/loop_viewer/svg/LoopComponentConverter';
+export { default as LoopLogs } from './components/loop_viewer/logs/LoopLogs';
+export { default as LoopPropertiesModal } from './components/dialogs/Loop/LoopPropertiesModal';
+export { default as LoopService } from './api/LoopService';
+export { default as LoopStatus } from './components/loop_viewer/status/LoopStatus';
+export { default as LoopUI } from './LoopUI';
+export { default as MenuBar } from './components/menu/MenuBar';
+export { default as NotFound } from './NotFound';
+export { default as OpenLoopModal } from './components/dialogs/Loop/OpenLoopModal';
+export { default as OperationalPolicyModal } from './components/dialogs/OperationalPolicy/OperationalPolicyModal';
+export { default as PerformActions } from './components/dialogs/PerformActions';
+export { default as RefreshStatus } from './components/dialogs/RefreshStatus';
+export { default as TemplateMenuService } from './api/TemplateMenuService';
+export { default as UserInfoModal } from './components/dialogs/UserInfoModal';
+export { default as UserService } from './api/UserService';
+export { default as ViewBlueprintMicroServiceTemplatesModal } from './components/dialogs/Tosca/ViewBlueprintMicroServiceTemplatesModal';
+export { default as ViewToscaPolicyModal } from './components/dialogs/Tosca/ViewToscaPolicyModal';
