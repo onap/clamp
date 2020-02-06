b37fda57d0bd9bda6ed7e7f3709b074ab4b7eeb2
WeberLaurent <weberlaurent23@gmail.com>
2020-02-06 10:23:01 +0100

    Rework tosca converter
    New code to convert the Policy Tosca Yaml to Json Schema for the Clamp
    UI
    
    Issue-ID: CLAMP-253
    Change-Id: I10c1d3d81c5feae402773de94bd88c190ec68645
    Signed-off-by: WeberLaurent <weberlaurent23@gmail.com>

---
+++ src/test/java/org/onap/clamp/clds/tosca/update/ArrayFieldTest.java
@@ -0,0 +1,42 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CLAMP
+ * ================================================================================
+ * Copyright (C) 2020 AT&T Intellectual Property. All rights
+ *                             reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+package org.onap.clamp.clds.tosca.update;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.JsonArray;
+
+import junit.framework.TestCase;
+
+public class ArrayFieldTest extends TestCase {
+
+    public void testDeploy() throws IOException {
+        TemplateManagement templateManagement = new TemplateManagement("finalMODIFIED.yaml","templates.properties");
+        Component component = templateManagement.getComponents().get("onap.datatype.controlloop.Actor");
+        Property property = component.getProperties().get("actor");
+        ArrayField arrayParser = new ArrayField((ArrayList<Object>) property.getItems().get("default"));
+        JsonArray toTest = arrayParser.deploy();
+        String reference = "[1,\"String\",5.5,true]";
+        assertEquals(reference, String.valueOf(toTest));
+    }
+}
