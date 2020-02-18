55ea66ed6ea90f651bb93b8924e1cf6ed1c2e910
xuegao <xg353y@intl.att.com>
2020-02-18 14:20:23 +0100

    Create get Pdp Groups flow
    Create a camel flow to get the list of Pdp Groups info from Policy.
    
    Issue-ID: CLAMP-644
    Change-Id: I6427202cc0186cd85428d5d25b28a8622e4d7ca4
    Signed-off-by: xuegao <xg353y@intl.att.com>

--- src/main/java/org/onap/clamp/loop/template/PolicyModelId.java
+++ src/main/java/org/onap/clamp/loop/template/PolicyModelId.java
@@ -90,4 +90,42 @@ public class PolicyModelId implements Serializable {
     public void setVersion(String version) {
         this.version = version;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((policyModelType == null) ? 0 : policyModelType.hashCode());
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        PolicyModelId other = (PolicyModelId) obj;
+        if (policyModelType == null) {
+            if (other.policyModelType != null) {
+                return false;
+            }
+        } else if (!policyModelType.equals(other.policyModelType)) {
+            return false;
+        }
+        if (version == null) {
+            if (other.version != null) {
+                return false;
+            }
+        } else if (!version.equals(other.version)) {
+            return false;
+        }
+        return true;
+    }
 }
