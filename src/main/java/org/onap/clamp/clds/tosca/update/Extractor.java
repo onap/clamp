5bc9dca008ffc41eb885a1ac2575acfa3bc787ea
sebdet <sebastien.determe@intl.att.com>
2020-03-04 15:47:39 -0800

    Rework tosca converter
    New code to convert the Policy Tosca Yaml to Json Schema for the Clamp
    UI
    
    Issue-ID: CLAMP-647
    Signed-off-by: sebdet <sebastien.determe@intl.att.com>
    Change-Id: Id15ddedc1910f6a40bf6e407b34e343e00135571

--- src/main/java/org/onap/clamp/clds/tosca/update/Extractor.java
+++ src/main/java/org/onap/clamp/clds/tosca/update/Extractor.java
@@ -23,19 +23,24 @@
 
 package org.onap.clamp.clds.tosca.update;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 import org.yaml.snakeyaml.Yaml;
 
 public class Extractor {
-
-    private LinkedHashMap<String, Component> allItems = new LinkedHashMap<>();
+    private LinkedHashMap<String, Component> allItems;
     private String source;
+    private String nativeComponent;
 
     @SuppressWarnings("unchecked")
-    public Extractor(String toParse) {
+    public Extractor(String toParse, String nativeComponent) throws IOException {
+
         this.source = toParse;
+        this.nativeComponent = nativeComponent;
+        allItems = new LinkedHashMap<String, Component>();
         getAllAsMaps();
+
     }
 
     public LinkedHashMap<String, Component> getAllItems() {
@@ -64,6 +69,12 @@ public class Extractor {
         LinkedHashMap<String, Object> policyTypes = file.get("policy_types");
         // Put the policies and datatypes in the same collection
         dataTypes.putAll(policyTypes);
+
+        Object contentNativeFile = yaml.load(nativeComponent);
+        LinkedHashMap<String, Object> dataTypesEmbedded =
+                ((LinkedHashMap<String, LinkedHashMap<String, Object>>) contentNativeFile).get("data_types");
+        dataTypes.putAll(dataTypesEmbedded);
+
         parseInComponent(dataTypes);
         return dataTypes;
     }
